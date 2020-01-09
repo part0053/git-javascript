e192dbc734478db0a4f346f744afdd183bccc201
part0053 <part0053@algonquinlive.com>
2020-01-09 17:43:59 -0500

    Initalizing number and processing it

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const name = 'Parth Chauhan'
 console.log(`Hello, my name is ${name}.`)
+
+const number = 20
+console.log(`The number is ${number}.\nNumber after processing is ${(((number*2)*5)/number)-7}.`)
